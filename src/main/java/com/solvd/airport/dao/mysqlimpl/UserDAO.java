846315fda184c1ee845b0420df92a91368880d3f
Anthony Davis <anthony.m.davis.26@gmail.com>
2021-12-19 05:59:46 -0800

    Implement ConnectionPool

--- src/main/java/com/solvd/airport/dao/mysqlimpl/UserDAO.java
+++ src/main/java/com/solvd/airport/dao/mysqlimpl/UserDAO.java
@@ -26,7 +26,6 @@ public class UserDAO extends AbstractMySQLDAO implements IUserDAO<User> {
     @Override
     public User getEntityById(long id) throws SQLException {
         Connection con = null;
-        Connection con;
         PreparedStatement st = null;
         ResultSet rs = null;
 
