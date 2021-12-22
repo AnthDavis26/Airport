3fc37001ac836957265c42df09323d338ec1d1ac
Anthony Davis <anthony.m.davis.26@gmail.com>
2021-12-22 05:02:07 -0800

    Hide database credentials, change ConnectionPool head retrieval method

--- src/main/java/com/solvd/airport/utils/ConnectionPool.java
+++ src/main/java/com/solvd/airport/utils/ConnectionPool.java
@@ -1,51 +1,63 @@
 package com.solvd.airport.utils;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 import java.util.concurrent.*;
 
 public class ConnectionPool {
     private static ConnectionPool instance;
-    private static ConcurrentLinkedQueue<Connection> pool;
+    private static LinkedBlockingQueue<Connection> pool;
     private static final int MAX_POOL_CAPACITY = 10;
-    private static int usedConnectionsCount = 0;
+    private static int existingConnectionsCount = 0;
     private static final Logger logger = LogManager.getLogger(ConnectionPool.class);
-    private static final String url = "52.59.193.212:3306";
-    private static final String user = "root";
-    private static final String password = "devintern";
+    private static String url;
+    private static String user;
+    private static String password;
 
     private ConnectionPool(){} // Prevent construction
 
     public static ConnectionPool getInstance() {
         if (instance == null)
+        {
             instance = new ConnectionPool();
 
-        return instance;
-    }
-
-    public Connection getConnection() throws InterruptedException {
-        if (usedConnectionsCount == 0)
-        {
             try {
-                Class.forName("com.mysql.jdbc.Driver");
-                Connection con = DriverManager.getConnection(url, user, password);
-                pool.add(con);
+                InputStream input = new FileInputStream("src/main/resources/db.properties");
+                Properties prop = new Properties();
+                prop.load(input);
+                url = prop.getProperty("url");
+                user = prop.getProperty("username");
+                password = prop.getProperty("password");
             } catch (Exception e) {
                 logger.error(e);
             }
         }
+        return instance;
+    }
 
-        while (usedConnectionsCount == MAX_POOL_CAPACITY)
-            pool.wait();
+    public synchronized Connection getConnection() throws InterruptedException {
+        if (existingConnectionsCount < MAX_POOL_CAPACITY) {
+            try {
+                existingConnectionsCount++;
+                return DriverManager.getConnection(url, user, password);
+            }
+            catch (SQLException e) {
+                logger.error(e);
+            }
+        }
 
-        usedConnectionsCount++;
-        return pool.poll();
+        return pool.take();
     }
 
     public void releaseConnection(Connection con) {
         pool.add(con);
-        usedConnectionsCount--;
     }
 }
