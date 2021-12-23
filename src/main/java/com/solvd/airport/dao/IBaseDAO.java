1edf869f3aba308ab2c01bd9d3a95c3dda59f393
Anthony Davis <anthony.m.davis.26@gmail.com>
2021-12-23 04:36:12 -0800

    Methods throw exceptions

--- src/main/java/com/solvd/airport/dao/IBaseDAO.java
+++ src/main/java/com/solvd/airport/dao/IBaseDAO.java
@@ -3,8 +3,8 @@ package com.solvd.airport.dao;
 import java.sql.SQLException;
 
 public interface IBaseDAO<T> {
-    void createEntity(T entity);
-    void updateEntity(T entity);
+    void createEntity(T entity) throws SQLException;
+    void updateEntity(T entity) throws SQLException;
     T getEntityById(long id) throws SQLException;
     void deleteEntityById(long id) throws SQLException;
 }
