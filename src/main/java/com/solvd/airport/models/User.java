8ba5da464ef0398928d90dafb89e3cecd9a0e841
Anthony Davis <anthony.m.davis.26@gmail.com>
2021-12-22 23:55:43 -0800

    Implement hierarchy

--- src/main/java/com/solvd/airport/models/User.java
+++ src/main/java/com/solvd/airport/models/User.java
@@ -1,13 +1,66 @@
 package com.solvd.airport.models;
 
-public class User {
-    private String name;
+import java.time.LocalDate;
 
-    public String getName() {
-        return name;
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthday;
+
+    public User() {
+        super();
     }
 
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
     }
 }
