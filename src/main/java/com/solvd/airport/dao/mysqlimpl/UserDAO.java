25694388b1507608706fbb3b26de48e59fcaccfb
Anthony Davis <anthony.m.davis.26@gmail.com>
2021-12-31 18:04:01 -0800

    implement createIdentity() and updateEntity()

--- src/main/java/com/solvd/airport/dao/mysqlimpl/UserDAO.java
+++ src/main/java/com/solvd/airport/dao/mysqlimpl/UserDAO.java
@@ -6,24 +6,59 @@ import com.solvd.airport.utils.ConnectionPool;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.List;
 
 public class UserDAO extends AbstractMySQLDAO implements IUserDAO<User> {
     private static final Logger logger = LogManager.getLogger(UserDAO.class);
-    private static final String GET_USER_BY_ID = "Select * from Users where id=?";
-    private static final String DELETE_USER_BY_ID = "Delete from Users where id=?";
+    private static final String INSERT_USER = "INSERT INTO Users (first_name, " +
+            "last_name, birthday) VALUES (?,?,?)";
+    private static final String UPDATE_USER_BY_ID = "UPDATE Users SET first_name=?, " +
+            "last_name=?, birthday=? WHERE id=?";
+    private static final String GET_USER_BY_ID = "SELECT * FROM Users where id=?";
+    private static final String GET_USERS_BY_AGE = "SELECT * FROM Users " +
+            "WHERE TIMESTAMPDIFF(YEAR, birthday, CURDATE())=?";
+    private static final String DELETE_USER_BY_ID = "DELETE FROM Users WHERE id=?";
 
     @Override
-    public void createEntity(User entity) {
+    public void createEntity(User user) throws SQLException {
+        Connection con = null;
+        PreparedStatement st = null;
 
+        try {
+            con = ConnectionPool.getInstance().getConnection();
+            st = con.prepareStatement(INSERT_USER);
+            st.setString(1, user.getFirstName());
+            st.setString(2, user.getLastName());
+            st.setDate(3, Date.valueOf(user.getBirthday()));
+            st.executeUpdate();
+        } catch (Exception e) {
+            logger.error(e);
+        } finally {
+            st.close();
+            ConnectionPool.getInstance().releaseConnection(con);
+        }
     }
 
     @Override
-    public void updateEntity(User entity) {
+    public void updateEntity(User user) throws SQLException {
+        Connection con = null;
+        PreparedStatement st = null;
 
+        try {
+            con = ConnectionPool.getInstance().getConnection();
+            st = con.prepareStatement(UPDATE_USER_BY_ID);
+            st.setString(1, user.getFirstName());
+            st.setString(2, user.getLastName());
+            st.setDate(3, Date.valueOf(user.getBirthday()));
+            st.setLong(4, user.getId());
+            st.executeUpdate();
+        } catch (Exception e) {
+            logger.error(e);
+        } finally {
+            st.close();
+            ConnectionPool.getInstance().releaseConnection(con);
+        }
     }
 
     @Override
@@ -31,6 +66,7 @@ public class UserDAO extends AbstractMySQLDAO implements IUserDAO<User> {
         Connection con = null;
         PreparedStatement st = null;
         ResultSet rs = null;
+        User user = null;
 
         try {
             con = ConnectionPool.getInstance().getConnection();
@@ -40,25 +76,10 @@ public class UserDAO extends AbstractMySQLDAO implements IUserDAO<User> {
         } catch (Exception e) {
             logger.error(e);
         } finally {
+            user = resultSetToUser(rs);
             rs.close();
             st.close();
             ConnectionPool.getInstance().releaseConnection(con);
-        }
-
-        return resultSetToUser(rs);
-    }
-
-    private User resultSetToUser(ResultSet rs) {
-        User user = new User();
-
-        try {
-            user.setId(rs.getLong("id"));
-            user.setFirstName(rs.getString("first_name"));
-            user.setLastName(rs.getString("last_name"));
-            user.setBirthday(rs.getDate("birthday").toLocalDate());
-        }
-        catch (Exception e) {
-            logger.error(e);
         }
 
         return user;
@@ -80,5 +101,21 @@ public class UserDAO extends AbstractMySQLDAO implements IUserDAO<User> {
             st.close();
             ConnectionPool.getInstance().releaseConnection(con);
         }
+    }
+
+    private User resultSetToUser(ResultSet rs) {
+        User user = new User();
+
+        try {
+            user.setId(rs.getLong("id"));
+            user.setFirstName(rs.getString("first_name"));
+            user.setLastName(rs.getString("last_name"));
+            user.setBirthday(rs.getDate("birthday").toLocalDate());
+        }
+        catch (Exception e) {
+            logger.error(e);
+        }
+
+        return user;
     }
 }
