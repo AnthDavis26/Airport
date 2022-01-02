c3550568c3ed0307ef9c813311ee15135bf69fce
Anthony Davis <anthony.m.davis.26@gmail.com>
2022-01-02 12:12:29 -0800

    implement getAllEntities()

--- src/main/java/com/solvd/airport/dao/IBaseDAO.java
+++ src/main/java/com/solvd/airport/dao/IBaseDAO.java
@@ -1,10 +1,12 @@
 package com.solvd.airport.dao;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface IBaseDAO<T> {
     void createEntity(T entity) throws SQLException;
     void updateEntity(T entity) throws SQLException;
     T getEntityById(long id) throws SQLException;
     void deleteEntityById(long id) throws SQLException;
+    List<T> getAllEntities() throws  SQLException;
 }
