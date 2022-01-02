632ce154146097c923bd6a94b74c3a8d76381ad9
Anthony Davis <anthony.m.davis.26@gmail.com>
2022-01-02 11:51:46 -0800

    Cleanup

--- src/main/java/com/solvd/airport/dao/IUserDAO.java
+++ src/main/java/com/solvd/airport/dao/IUserDAO.java
@@ -1,8 +1,13 @@
 package com.solvd.airport.dao;
 
+import com.solvd.airport.models.User;
+
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface IUserDAO<T> extends IBaseDAO<T> {
+    List<T> getUsersByFirstName(String firstName) throws SQLException;
+    List<T> getUsersByDOB(LocalDate dob) throws SQLException;
     List<T> getUsersByAge(int age) throws SQLException;
 }
