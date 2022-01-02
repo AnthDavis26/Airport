65a267778fd8881f02f27fe394d36a5a04f83c3d
Anthony Davis <anthony.m.davis.26@gmail.com>
2022-01-01 16:03:57 -0800

    Implement user service methods

--- src/main/java/com/solvd/airport/services/IUserService.java
+++ src/main/java/com/solvd/airport/services/IUserService.java
@@ -3,7 +3,11 @@ package com.solvd.airport.services;
 import com.solvd.airport.models.User;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface IUserService {
+    void createUser(User user) throws SQLException;
     User getUserById(long id) throws SQLException;
+    void deleteUserById(long id) throws SQLException;
+    List<User> getUsersByAge(int age) throws SQLException;
 }
--- src/main/java/com/solvd/airport/services/impl/UserService.java
+++ src/main/java/com/solvd/airport/services/impl/UserService.java
@@ -6,12 +6,28 @@ import com.solvd.airport.models.User;
 import com.solvd.airport.services.IUserService;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class UserService implements IUserService {
     private IUserDAO userDAO = new UserDAO();
 
     @Override
+    public void createUser(User user) throws SQLException {
+        userDAO.createEntity(user);
+    }
+
+    @Override
     public User getUserById(long id) throws SQLException {
         return (User) userDAO.getEntityById(id);
+    }
+
+    @Override
+    public void deleteUserById(long id) throws SQLException {
+        userDAO.deleteEntityById(id);
+    }
+
+    @Override
+    public List<User> getUsersByAge(int age) throws SQLException {
+        return userDAO.getUsersByAge(age);
     }
 }
