862159d94de1cf15858c755988963edbcede2eba
Anthony Davis <anthony.m.davis.26@gmail.com>
2022-01-12 11:12:17 -0800

    Correct list type to generic

--- src/main/java/com/solvd/airport/dao/IUserDAO.java
+++ src/main/java/com/solvd/airport/dao/IUserDAO.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface IUserDAO<T> extends IBaseDAO<T> {
     List<T> getUsersByFirstName(String firstName);
-    List<User> getUsersByLastName(String lastName);
+    List<T> getUsersByLastName(String lastName);
     List<T> getUsersByDOB(LocalDate dob);
     List<T> getUsersByAge(int age);
 }
