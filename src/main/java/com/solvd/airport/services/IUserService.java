c00cdbe297e5271ff29054c249cb788fc4d64e89
Anthony Davis <anthony.m.davis.26@gmail.com>
2022-01-16 12:22:11 -0800

    Remove functionality for reevaluation

--- src/main/java/com/solvd/airport/services/IUserService.java
+++ src/main/java/com/solvd/airport/services/IUserService.java
@@ -5,11 +5,5 @@ import com.solvd.airport.models.User;
 import java.time.LocalDate;
 import java.util.List;
 
-public interface IUserService extends IBaseService<User> {
-    User getUserById(long id);
-    void deleteUserById(long id);
-    List<User> getUsersByAge(int age);
-    List<User> getUsersByFirstName(String firstName);
-    List<User> getUsersByLastName(String lastName);
-    List<User> getUsersByDateOfBirth(LocalDate dob);
+public interface IUserService {
 }
