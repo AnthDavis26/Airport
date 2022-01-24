71762a1b0f1f1907a406c84f03836bfbcb3f16f4
Anthony Davis <anthony.m.davis.26@gmail.com>
2022-01-23 18:17:06 -0800

    JAXB and JSON demonstrations

--- src/main/java/com/solvd/airport/runner/Runner.java
+++ src/main/java/com/solvd/airport/runner/Runner.java
@@ -1,9 +1,11 @@
 package com.solvd.airport.runner;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.solvd.airport.dao.mysqlimpl.UserDAO;
-import com.solvd.airport.models.Users;
-import com.solvd.airport.services.impl.UserService;
-import com.solvd.airport.utils.ConnectionPool;
+import com.solvd.airport.models.User;
+import com.solvd.airport.utils.Wrapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,33 +13,52 @@ import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import java.io.File;
-import java.sql.Connection;
+import java.io.IOException;
+import java.util.List;
 
 public class Runner {
     private static final Logger LOGGER = LogManager.getLogger(Runner.class);
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
+        // JAXB with XML
         try {
-            JAXBContext jaxbContext = JAXBContext.newInstance(Users.class);
+            JAXBContext jaxbContext = JAXBContext.newInstance(Wrapper.class, User.class);
             Marshaller marshaller = jaxbContext.createMarshaller();
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
             Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-
             SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
             Schema userSchema = schemaFactory.newSchema(new File("src/main/resources/users.xsd"));
             unmarshaller.setSchema(userSchema);
 
-            Users users = new Users();
-            UserDAO userDAO = new UserDAO();
-            users.setUsers(userDAO.getAllEntities());
-            LOGGER.info(users.getUsers());
+            Wrapper<User> users = (Wrapper<User>) unmarshaller.unmarshal(
+                    new StreamSource("src/main/resources/users.xml"), Wrapper.class).getValue();
+
+            for (User u : users.getAll())
+            {
+                //new UserDAO().createEntity(u);
+                LOGGER.info(u);
+            }
             marshaller.marshal(users, new File("src/main/resources/userTestOutput.xml"));
-            userDAO.deleteEntityById(14);
         } catch (Exception e) {
             LOGGER.error(e);
         }
+
+        // JSON
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        objectMapper.registerModule(new JavaTimeModule());
+
+        // File json = FileUtils.getFile("src/main/resources/users.json");
+        // List<User> users = objectMapper.readValue(json, new TypeReference<ArrayList<User>>(){});
+        List<User> users = new UserDAO().getAllEntities();
+
+        for (User u : users)
+            LOGGER.info(u);
+
+        objectMapper.writeValue(new File("src/main/resources/output.json"), users);
     }
 }
